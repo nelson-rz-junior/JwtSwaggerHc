f72d9073b247f295c9b3aeb1ab9f5dfd180e900b
Nelson Roza Junior <jr.1403@hotmail.com>
2021-03-28 17:19:00 -0300

    Add hub file

---
+++ JwtSwaggerHc.API/Hubs/NotificationHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace JwtSwaggerHc.API.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public async Task SendMessage(IHubContext<NotificationHub> hubContext, string method, string call, string message)
+        {
+            await hubContext.Clients.All.SendAsync(method, call, message);
+        }
+    }
+}
